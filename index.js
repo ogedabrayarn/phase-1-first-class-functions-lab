6bdc4753bd8c933a96840c87502898583bc0d49a
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-08-17 21:37:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
 const returnFirstTwoDrivers = function (drivers) {
     return drivers.slice(0, 2);
   };
+  console.log(returnFirstTwoDrivers(drivers))
   
   const returnLastTwoDrivers = function (drivers) {
     return drivers.slice(-2);
   };
+  console.log(returnLastTwoDrivers(drivers))
   
   const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
   
@@ -19,6 +22,7 @@ const returnFirstTwoDrivers = function (drivers) {
   
   const fareTripler = createFareMultiplier(3);
   
-  const selectDifferentDrivers = function (drivers, driversToReturn) {
-    return driversToReturn(drivers);
+  const selectDifferentDrivers = function (drivers, selectingDrivers) {
+    return selectingDrivers(drivers);
   };
+  console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers));
